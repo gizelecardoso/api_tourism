c4b5b10b458079b72fc2bceaf8eefebeb28c1d86
Gizele Santos <gizele.ccardoso@gmail.com>
2024-03-13 19:10:19 -0300

    feat: Add second api call amadeus

--- app/interactors/apis/find_tourism_infos.rb
+++ app/interactors/apis/find_tourism_infos.rb
@@ -4,18 +4,22 @@ module Apis
   # Class responsible for call amadeus api and return some tourism places
   class FindTourismInfos
     include Interactor
+    include HTTParty
+    base_uri 'https://test.api.amadeus.com/v1/reference-data/locations/pois'
 
     def call
-      context.tourism = [
-        {
-          name: 'Museu do Louvre',
-          category: 'museu'
-        },
-        {
-          name: 'Torre Eiffel',
-          category: 'monumento'
-        }
-      ]
+      context.geocoder = { latitude: 52.5186, longitude: 13.4081 }
+      context.tourism = []
+
+      response = self.class.get("?latitude=#{context.geocoder[:latitude]}&longitude=#{context.geocoder[:longitude]}",
+                                headers: { 'Authorization' => "Bearer #{ENV.fetch('AMADEUS_TOKEN')}" })
+
+      res_parsed = JSON.parse(response.body)
+      return [] if res_parsed.blank? || res_parsed['data'].blank?
+
+      res_parsed['data'].each do |res|
+        context.tourism << { name: res['name'], category: res['category'] }
+      end
     end
   end
 end
--- spec/interactors/apis/find_tourism_infos_spec.rb
+++ spec/interactors/apis/find_tourism_infos_spec.rb
@@ -4,21 +4,28 @@ require 'rails_helper'
 
 RSpec.describe Apis::FindTourismInfos, type: :interactor do
   describe '#call' do
-    it 'returns points of tourism of some place' do
-      response = described_class.call
-
-      expect(response.tourism).to eq(
-        [
-          {
-            name: 'Museu do Louvre',
-            category: 'museu'
-          },
-          {
-            name: 'Torre Eiffel',
-            category: 'monumento'
-          }
+    context 'when has some information' do
+      it 'returns points of tourism of some place' do
+        lista = [
+          { category: 'SIGHTS', name: 'Berliner Dom' },
+          { category: 'SIGHTS', name: 'Pergamon Museum' },
+          { category: 'SIGHTS', name: 'DDR Museum' },
+          { category: 'RESTAURANT', name: 'Alexanderplatz' }
         ]
-      )
+
+        allow(Apis::FindTourismInfos).to receive(:call).and_return(lista)
+        response = described_class.call
+
+        expect(response).to eq(lista)
+      end
+    end
+    context 'when hasnt any information' do
+      it 'returns points of tourism of some place' do
+        allow(Apis::FindTourismInfos).to receive(:call).and_return([])
+        response = described_class.call
+
+        expect(response).to eq([])
+      end
     end
   end
 end
